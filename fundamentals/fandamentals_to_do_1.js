bb15eda18d843f6a1c10b44c4f3f1acde8f2f870
Patrick-Hillme <path1727@gmail.com>
2023-09-19 23:22:08 -0500

    Refactored fundamentals_to_do_1

--- fundamentals/fandamentals_to_do_1.js
+++ fundamentals/fandamentals_to_do_1.js
@@ -1,14 +1,22 @@
 // Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
 
-let myNumber = 42;
-let myName = "Patrick"
-let temp = ""
-const swap = () => {
-    temp = myNumber;
-    myNumber = myName;
-    myName = temp;
-    results = `Number: ${myNumber} Name: ${myName}`
-    return results;
-}
+// let myNumber = 42;
+// let myName = "Patrick"
+// let temp = ""
+// const swap = () => {
+//     temp = myNumber;
+//     myNumber = myName;
+//     myName = temp;
+//     results = `Number: ${myNumber} Name: ${myName}`
+//     return results;
+// }
 
-console.log(swap());
+// console.log(swap());
+
+//! Refactoring:
+let myNumber = 42;
+let myName = "Patrick";
+
+[myNumber, myName] = [myName, myNumber];
+
+console.log(`Number: ${myNumber} Name: ${myName}`);
