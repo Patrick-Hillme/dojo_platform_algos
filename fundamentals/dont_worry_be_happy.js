674521573a92442b6e58979b25661fc23ef1d564
Patrick-Hillme <path1727@gmail.com>
2023-09-25 23:07:52 -0500

    Did two algos

---
+++ fundamentals/dont_worry_be_happy.js
@@ -0,0 +1,9 @@
+//todo Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
+
+function beCheerful() {
+    for (let i = 1; i <= 98; i++) {
+        console.log("good morning!")
+    }
+}
+
+beCheerful();
